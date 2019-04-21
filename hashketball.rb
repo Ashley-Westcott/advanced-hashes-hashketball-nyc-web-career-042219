92eaeca75e1808e1e1201fae4795bb1aa18412ee
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-21 22:07:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -156,5 +156,15 @@ end
   points
 end
 
+def num_points_scored(name)
+  game_hash.each do | team, team_data_hash |
+    team_data_hash.each do | player, player_data_hash |
+      if player.include?(name) 
+       return game_hash[team][attribute][name][:points]
+      end
+    end
+  end
+end
+
 
      
